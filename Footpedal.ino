88dc902fbf66ec4a1873eba3f87de153ed7d1149
Simon Alveteg <simon.alveteg@gmail.com>
2022-10-08 12:11:22 +0200

    WIP: Defining and adding buttons individually, adding callback in loop.

--- Footpedal.ino
+++ Footpedal.ino
@@ -6,15 +6,15 @@ const int Pins[nbrButtons] = {6,7,8,9,10,16,14,15,18,19,20,21}; // array of pins
 OneButton buttons[nbrButtons];
 const int ledPin = 1;
 
-OneButton b = OneButton(21,true,true);
-OneButton b1 = OneButton(20,true,true);
-OneButton b2 = OneButton(19,true,true);
-OneButton b3 = OneButton(18,true,true);
-OneButton b4 = OneButton(17,true,true);
-
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
+
+  OneButton b = OneButton(21,true,true);
+  OneButton b1 = OneButton(20,true,true);
+  OneButton b2 = OneButton(19,true,true);
+  OneButton b3 = OneButton(18,true,true);
+  OneButton b4 = OneButton(15,true,true);
 
 
   buttons[0] = b;
@@ -23,48 +23,20 @@ void setup() {
   buttons[3] = b3;
   buttons[4] = b4;
 
-  
-  buttons[0].attachClick([](){
-    Serial.println("b pressed!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  });
-  buttons[1].attachClick([](){
-    Serial.println("b1 pressed!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  });
-  buttons[2].attachClick([](){
-    Serial.println("b2 pressed!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  });
-  buttons[3].attachClick([](){
-    Serial.println("b3 pressed!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  });
-  buttons[4].attachClick([](){
-    Serial.println("b4 pressed!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  });
+  for (int i = 0; i < nbrButtons; i++) {
+      buttons[i].attachClick([](){
+      Serial.println("b pressed!");
+      digitalWrite(ledPin, HIGH);
+      delay(50);
+      digitalWrite(ledPin, LOW);
+    });  
+  }
 }
 
 void loop() {
   // loop through buttons and tick
-  for (OneButton button: buttons) {
-    button.tick();
+  for (int i = 0; i < nbrButtons; i++) {
+    buttons[i].tick(); 
   }
-  //buttons[0].tick();
-  //buttons[1].tick();
-  //buttons[2].tick();
-  //buttons[3].tick();
-  buttons[4].tick();
   //Serial.println(buttons[0].isLongPressed());
 }
