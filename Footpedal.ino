72b47075ca33e73b3c523fd7d7569ed06c91e34f
Simon Alveteg <simon.alveteg@gmail.com>
2022-10-08 13:22:38 +0200

    Extracted lambda function, send iterator variable to it

--- Footpedal.ino
+++ Footpedal.ino
@@ -4,29 +4,26 @@
 const int nbrButtons = 12;
 const int Pins[nbrButtons] = {6,7,8,9,10,16,14,15,18,19,20,21}; // array of pins I want to use
 OneButton buttons[nbrButtons];
-const int ledPin = 1;
+
+void singlePress(void *s) {
+  Serial.print("Button ");
+  Serial.print((char *)s);
+  Serial.println(" pressed!");
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-
 
   for (int i = 0; i < nbrButtons; i++) {
+    int s = i;
     buttons[i] = OneButton(Pins[i],true,true);
-
-    buttons[i].attachClick([](){
-      Serial.println("b pressed!");
-      digitalWrite(ledPin, HIGH);
-      delay(50);
-      digitalWrite(ledPin, LOW);
-    });  
+    buttons[i].attachClick(singlePress, i);
   }
 }
 
 void loop() {
-  // loop through buttons and tick
   for (int i = 0; i < nbrButtons; i++) {
-    buttons[i].tick(); 
+    bool isPressed = (digitalRead(Pins[i]) == LOW);
+    buttons[i].tick(isPressed); 
   }
-  //Serial.println(buttons[0].isLongPressed());
 }
